196091e8def6c8bb5f7cabc46316d4e8f7b7fea0
TobiasM <tobias.meyer@maibornwolff.de>
2020-11-17 20:55:31 +0100

    Added tests for Context log function

--- DemoFunction/index.test.ts
+++ DemoFunction/index.test.ts
@@ -1,37 +1,40 @@
 import httpTrigger from "./index";
 import { Context } from "@azure/functions";
 
-const context = function () {
-    return {log: jest.fn()} as unknown as Context
-}()
+describe("Test for Demo Function", () => {
+  let context: Context;
 
-describe('Test for Demo Function',   () => {
-    it('should return a 200',  async () => {
+  beforeEach(() => {
+    context = ({ log: jest.fn() } as unknown) as Context;
+  });
 
-        // Arrange
-        const request = {
-            body: {name: "John"},
-        };
+  it("should return a 200", async () => {
+    // Arrange
+    const request = {
+      body: { name: "John" },
+    };
 
-        // Action
-        await httpTrigger(context, request);
+    // Action
+    await httpTrigger(context, request);
 
-        // Assertion
-        expect(context.res.status).toEqual(200)
-    });
+    // Assertion
+    expect(context.log).toBeCalledTimes(1);
+    expect(context.res.status).toEqual(200);
+  });
 
-    it('should return a result string with the name given in the request body',  async () => {
+  it("should return a result string with the name given in the request body", async () => {
+    // Arrange
+    const request = {
+      body: { name: "John" },
+    };
+    const resultString =
+      "Hello, John. This HTTP triggered function executed successfully.";
 
-        // Arrange
-        const request = {
-            body: {name: "John"},
-        };
-        const resultString = "Hello, John. This HTTP triggered function executed successfully."
+    // Action
+    await httpTrigger(context, request);
 
-        // Action
-        await httpTrigger(context, request);
-
-        // Assertion
-        expect(context.res.body).toEqual(resultString);
-    });
+    // Assertion
+    expect(context.log).toBeCalledTimes(1);
+    expect(context.res.body).toEqual(resultString);
+  });
 });
